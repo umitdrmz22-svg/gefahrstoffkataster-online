4a1f6f5d6ab8485a8d31a7319a721f2c0d584e4d
umitdrmz22-svg <umitdrmz22@gmail.com>
2026-08-06 10:50:44 +0200

    Gemeinsame Produktivkonfiguration verwenden

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,12 @@
-window.APP_CONFIG = Object.freeze({
-  supabaseUrl: '',
-  supabasePublishableKey: '',
-  appName: 'Gefahrstoffkataster Online'
-});
+'use strict';
+(function(){
+  const shared=window.EHS_PLATFORM_CONFIG||{};
+  const current=window.APP_CONFIG||{};
+  window.APP_CONFIG=Object.freeze({
+    supabaseUrl:shared.supabaseUrl||current.supabaseUrl||'',
+    supabasePublishableKey:shared.supabasePublishableKey||shared.supabaseAnonKey||current.supabasePublishableKey||'',
+    appName:'Gefahrstoffkataster Online',
+    productionOnly:true,
+    baAppUrl:shared.baAppUrl||current.baAppUrl||''
+  });
+})();
