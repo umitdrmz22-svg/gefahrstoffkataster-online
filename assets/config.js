23e600f523d884feabc636141083720b58883a23
umitdrmz22-svg <umitdrmz22@gmail.com>
2026-08-07 14:24:45 +0200

    Connect Gefahrstoffkataster to BA Studio

--- assets/config.js
+++ assets/config.js
@@ -4,11 +4,12 @@
   const current=window.APP_CONFIG||{};
   const defaultSupabaseUrl='https://rqvcbjomrjccyuchxpuh.supabase.co';
   const defaultPublishableKey='sb_publishable_iKh-ZfqV3iJpr_9b7SErEA_XhrqnSsY';
+  const defaultBaAppUrl='https://umitdrmz22-svg.github.io/ba-generator/';
   window.APP_CONFIG=Object.freeze({
     supabaseUrl:shared.supabaseUrl||current.supabaseUrl||defaultSupabaseUrl,
     supabasePublishableKey:shared.supabasePublishableKey||shared.supabaseAnonKey||current.supabasePublishableKey||defaultPublishableKey,
     appName:'Gefahrstoffkataster Online',
     productionOnly:true,
-    baAppUrl:shared.baAppUrl||current.baAppUrl||''
+    baAppUrl:shared.baAppUrl||current.baAppUrl||defaultBaAppUrl
   });
 })();
